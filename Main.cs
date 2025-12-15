e0a01f2b906a55cd84572d2c7d9b02b8ad085ff6
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-15 09:15:58 +0200

    start out scene changing

--- Main.cs
+++ Main.cs
@@ -3,11 +3,23 @@ using System;
 
 public partial class Main : Node
 {
+	Node rootNode;
 	Node currentScene;
+	Control globalHud;
 	
 	public override void _Ready()
 	{
-		currentScene = GetNode<Node>("Main/Scene");
+		currentScene = GetNode<Node>("Scene");
+		globalHud = GetNode<Control>("GlobalHud");
+		
+		SetScene("res://Scenes/Battle.tscn");
+	}
+	
+	public void SetScene(string ScenePath)
+	{
+		Node newScene = ResourceLoader.Load<PackedScene>(ScenePath).Instantiate();
+		
+		currentScene.AddChild(newScene);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
