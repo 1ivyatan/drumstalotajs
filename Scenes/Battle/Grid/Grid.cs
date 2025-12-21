631544b09ef0c4bb4421403acf7279052fba48bb
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-21 12:43:26 +0200

    detect placeholder clicks

--- Scenes/Battle/Grid/Grid.cs
+++ Scenes/Battle/Grid/Grid.cs
@@ -7,9 +7,27 @@ public partial class Grid : Node2D
 	{
 		
 	}
-
-	public override void _Process(double delta)
+	
+	public override void _Input(InputEvent @event)
 	{
-		
+		if (@event is InputEventMouseButton eventButton)
+		{
+			if (eventButton.Pressed)
+			{
+	   			var spaceState = GetWorld2D().DirectSpaceState;
+				var query = new PhysicsPointQueryParameters2D();
+				query.Position = GetGlobalMousePosition();
+				query.CollideWithAreas = true;
+				
+				var result = spaceState.IntersectPoint(query, 1);
+				
+				if (result.Count > 0)
+				{
+					Area2D collider = (Area2D)result[0]["collider"];
+						
+					GD.Print(collider);
+				}
+			}
+		}
 	}
 }
