8894c9faf57e400b5148148ce08d9edc3719afd7
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-22 23:33:45 +0200

    keep track of entities

---
+++ Scenes/Battle/Grid/Entities.cs
@@ -0,0 +1,30 @@
+using Godot;
+using Godot.Collections;
+using System;
+using System.Linq;
+
+public partial class Entities : TileMapLayer
+{
+	public enum EntityType
+	{
+		DevicePlaceholder = 0,
+		Device = 1
+	}
+	
+	Dictionary<int, Vector2[]> entities;
+	
+	public void UpdateCount(EntityType entityTypeId)
+	{
+		entities[(int)entityTypeId] = GetUsedCellsById(0, new Vector2I(0, 0), (int)entityTypeId).Select(e => new Vector2(e.X, e.Y)).ToArray();
+	}
+	
+	public override void _Ready()
+	{
+		entities = new Dictionary<int, Vector2[]>();
+		
+		foreach (EntityType entityTypeId in Enum.GetValues(typeof(EntityType)))
+		{
+			UpdateCount(entityTypeId);
+		}
+	}
+}
