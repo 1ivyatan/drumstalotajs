e3941c27a4206e523b02a7a66f2a5e6be50ae236
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-25 16:27:22 +0200

    load controls depending on stage

--- Scenes/Battle/Battle.cs
+++ Scenes/Battle/Battle.cs
@@ -6,15 +6,43 @@ public partial class Battle : VBoxContainer
 	[Signal]
 	public delegate void LevelSelectEventHandler();
 	
+	Node headerContainer;
+	Node footerContainer;
+	
 	public override void _Ready()
 	{
-		//Button pauseButton = GetNode<Button>("FooterContainer/ButtonContainer/Exit");
-		//pauseButton.Connect("pressed", new Callable(this, nameof(ToLevelSelect)));
 	}
 	
 	void StageChanged(string name)
 	{
-		GD.Print("it's now " + name);
+		headerContainer = GetNode<PanelContainer>("HeaderContainer");
+		footerContainer = GetNode<PanelContainer>("FooterContainer");
+		
+		foreach (Node child in headerContainer.GetChildren())
+		{
+			child.QueueFree();
+			headerContainer.RemoveChild(child);
+		}
+		
+		foreach (Node child in footerContainer.GetChildren())
+		{
+			child.QueueFree();
+			footerContainer.RemoveChild(child);
+		}
+		
+		string path = $"res://Scenes/Battle/Controls/{name}";
+		
+		if (ResourceLoader.Exists($"{path}/Header.tscn")) 
+		{
+			Node header = ResourceLoader.Load<PackedScene>($"{path}/Header.tscn").Instantiate();
+			headerContainer.AddChild(header);
+		}
+		
+		if (ResourceLoader.Exists($"{path}/Footer.tscn"))
+		{
+			Node footer = ResourceLoader.Load<PackedScene>($"{path}/Footer.tscn").Instantiate();
+			footerContainer.AddChild(footer);
+		}
 	}
 
 	void ToLevelSelect() {
