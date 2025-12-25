a342935188aa4a7a4167fb2f356de33a4c469249
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-25 23:13:07 +0200

    label updates on addition

--- Scenes/Battle/Widgets/DevicePlacing/DevicePlacingHeader.cs
+++ Scenes/Battle/Widgets/DevicePlacing/DevicePlacingHeader.cs
@@ -3,13 +3,26 @@ using System;
 
 public partial class DevicePlacingHeader : Widget
 {
+	Label addedCount;
+	
 	protected override void LoadWidget()
 	{
-		
+		addedCount = GetNode<Label>("HFlowContainer/AddedCount");
 	}
 	
 	public void SetLabels(int deviceCount)
 	{
-		
+		switch (deviceCount)
+		{
+			case 0:
+				addedCount.Text = "Nav nekas ievietots";
+				break;
+			case 1:
+				addedCount.Text = "1 ievietots";
+				break;
+			default:
+				addedCount.Text = $"{deviceCount} ievietoti";
+				break;
+		}
 	}
 }
