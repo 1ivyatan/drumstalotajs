1254109246be9df57cdf15af1ae8356ec7fbd203
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-30 00:33:32 +0200

    get selector event

--- Scenes/Battle/Stage/Stages/DeviceAdjustment.cs
+++ Scenes/Battle/Stage/Stages/DeviceAdjustment.cs
@@ -8,22 +8,51 @@ public partial class DeviceAdjustment : Stage
 	private EntityLayer entityLayer;
 	private TopPanel topPanel;
 	
+	private Callable entitySelectedCall;
+	
+	private PanelContainer deviceAdjustmentPanel;
+	
+	private void HideAdjustmentPanel()
+	{
+		
+	}
+	
+	private void ShowAdjustmentPanel()
+	{
+		
+	}
+	
+	private void ClickedOnDevice(int entityType, Vector2I position)
+	{
+		GD.Print("selected!!!!!!");
+	}
+	
 	public override void _Ready()
 	{
 		this.map = this.GetNode<Node2D>("../../MapContainer/Map");
 		this.selector = this.map.GetNode<Node2D>("Selector") as Selector;
 		this.entityLayer = this.map.GetNode<TileMapLayer>("Grid/EntityLayer") as EntityLayer;
 		this.topPanel = this.GetNode("../../../TopPanel") as TopPanel;
+		this.deviceAdjustmentPanel = this.GetNode<PanelContainer>("DeviceAdjustmentPanel");
 	
 		this.selector.Layer = Selector.SelectorLayer.Entity;
 		this.selector.EntityTypeFilter = [Entity.EntityType.Device];
 		this.selector.SelectorMode = Selector.SelectorFilterMode.Fitlered;
-	
+		
+		this.entitySelectedCall = new Callable(this, nameof(ClickedOnDevice));
+
 		this.topPanel.SetTopbarLabel("Ierīču koriģēšana");
 		
 		if (this.entityLayer.EntityCollections[Entity.EntityType.DeviceMarker].Count > 0)
 		{
 			this.entityLayer.EraseEntitiesByType(Entity.EntityType.DeviceMarker);	
 		}
+		
+		this.selector.Connect("EntitySelected", this.entitySelectedCall);
+	}
+	
+	public override void _ExitTree()
+	{
+		this.selector.Disconnect("EntitySelected", this.entitySelectedCall);
 	}
 }
