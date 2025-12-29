46dd16e6a9a1ddab667e5eaabc9250e0eb1204af
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-29 23:53:28 +0200

    forgot to assign pointer to top panel

--- Scenes/Battle/Stage/Stages/DeviceAdjustment.cs
+++ Scenes/Battle/Stage/Stages/DeviceAdjustment.cs
@@ -13,6 +13,7 @@ public partial class DeviceAdjustment : Stage
 		this.map = this.GetNode<Node2D>("../../MapContainer/Map");
 		this.selector = this.map.GetNode<Node2D>("Selector") as Selector;
 		this.entityLayer = this.map.GetNode<TileMapLayer>("Grid/EntityLayer") as EntityLayer;
+		this.topPanel = this.GetNode("../../../TopPanel") as TopPanel;
 	
 		this.selector.Layer = Selector.SelectorLayer.Entity;
 		this.selector.EntityTypeFilter = [Entity.EntityType.Device];
