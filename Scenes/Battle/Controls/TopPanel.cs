ee002dd9da548aa716546356fe531ec53a8090da
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2025-12-29 22:27:16 +0200

    add methods to topbar labels

--- Scenes/Battle/Controls/TopPanel.cs
+++ Scenes/Battle/Controls/TopPanel.cs
@@ -3,4 +3,24 @@ using System;
 
 public partial class TopPanel : PanelContainer
 {
+	private Button exitButton;
+	private Label deviceCountLabel;
+	private Label topbarLabel;
+	
+	public void SetDeviceCountLabel(string text)
+	{
+		this.deviceCountLabel.Text = text;
+	}
+	
+	public void SetTopbarLabel(string text)
+	{
+		this.topbarLabel.Text = text;
+	}
+	
+	public override void _Ready()
+	{
+		this.exitButton = this.GetNode<Button>("OptionsStatsGrid/ExitButton");
+		this.deviceCountLabel = this.GetNode<Label>("OptionsStatsGrid/StatsContainer/DeviceCount");
+		this.topbarLabel = this.GetNode<Label>("FlyingLabel");
+	}
 }
