49f9ff3d8126456371a236a6507c39b5576ca9e6
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-01-12 23:03:57 +0200

    have a hard time figuring out how to move a projectile

--- Scenes/Battle/Map/Entity/Entities/Devices/Projectile.cs
+++ Scenes/Battle/Map/Entity/Entities/Devices/Projectile.cs
@@ -15,6 +15,12 @@ public partial class Projectile : Node2D
 		private set;
 	} = 0;
 	
+	public Vector2 TargetPosition
+	{
+		get;
+		private set;	
+	}
+	
 	public void SetTrajectory(float azimuth, Vector2 spawnPosition)
 	{
 		this.Azimuth = azimuth;
@@ -25,10 +31,26 @@ public partial class Projectile : Node2D
 	
 	public void Fire()
 	{
-		
+		this.TargetPosition = new Vector2(200, 200);
+		//float distance = GlobalTransform.Origin.DistanceTo(this.TargetPosition);
 	}
 	
-	public override void _EnterTree()
+	
+	public override void _PhysicsProcess(double delta)
+	{
+			//this.body.Velocity = Position.DirectionTo(_target) * 400;
+			//GD.Print(this.body.Velocity);
+			Position += Transform.X * (float)delta * 200;
+			
+			
+			
+		// LookAt(_target);
+	}
+	
+	
+	
+	
+	public override void _Ready()
 	{
 		Fire();
 	}
