b12739e381d2c92f1d5f1416d58e54043c1d0980
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-01-24 20:48:06 +0200

    get height

--- Scenes/Battle/Map/Entity/Entities/Devices/Projectile.cs
+++ Scenes/Battle/Map/Entity/Entities/Devices/Projectile.cs
@@ -8,6 +8,9 @@ public partial class Projectile : Area2D
 	
 	private ProjectileMotion projectileMotion;
 	
+	private TileMapLayer groundLayer;
+	private Tween tween;
+	
 	public void SetTrajectory(float azimuth, float initialVelocity, float angle, Vector2 spawnPosition)
 	{
 		this.projectileMotion = new ProjectileMotion(spawnPosition, (double)azimuth, (double)angle, (double)initialVelocity);
@@ -18,16 +21,29 @@ public partial class Projectile : Area2D
 	
 	public void Fire()
 	{
-		Tween tween = this.CreateTween();
-		tween.TweenProperty(this, "position", this.projectileMotion.MapMovement.EndPosition, this.projectileMotion.Movement.Time);
-		tween.TweenCallback(Callable.From(() => {
+		this.tween = this.CreateTween();
+		this.tween.SetProcessMode(0); /* physics process */
+		this.tween.SetTrans((Tween.TransitionType)1);
+		this.tween.TweenProperty(this, "position", this.projectileMotion.MapMovement.EndPosition, this.projectileMotion.Movement.Time);
+		this.tween.TweenCallback(Callable.From(() => {
 			this.EmitSignal(SignalName.ProjectileLanded);
 			this.QueueFree();
 		}));
 	}
 	
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2I position = this.groundLayer.LocalToMap(this.Position);
+		TileData data = this.groundLayer.GetCellTileData(position);
+		
+		var height = data.GetCustomData("height");
+		GD.Print(height);
+	}
+	
 	public override void _Ready()
 	{
+		/* !!!!!!!!! */
+		this.groundLayer = this.GetNode<TileMapLayer>("../../GroundLayer");
 		GD.Print(this.projectileMotion.Movement.Range);
 		Fire();
 	}
