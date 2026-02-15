f057eb8f54e1aedccbc2f6e623b1b0f45db95977
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-15 22:34:00 +0200

    preparing structure

---
+++ Source/Battle/Entities/Device/DeviceProperties.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+namespace Drumstalotajs.Battle.Entities
+{
+	public partial class Device : Entity
+	{
+		private class DeviceProperties
+		{
+			public struct Angle
+			{
+				
+			}
+			
+			public struct Traverse
+			{
+				
+			}
+			
+			public struct Range
+			{
+				
+			}
+			
+			public struct Height
+			{
+				
+			}
+			
+			public float Velocity {  }
+			
+			public DeviceProperties()
+			{
+				
+			}
+		}
+	}
+}
