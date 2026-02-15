15624d40b4df8f91a7c0f0914172236d2aaeae19
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-15 23:11:25 +0200

    start out projectile properties

---
+++ Source/Battle/Entities/Device/ProjectileProperties.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+using Godot;
+using System;
+
+namespace Drumstalotajs.Battle.Entities
+{
+	public partial class ProjectileProperties
+	{
+		struct BlastRadius
+		{
+			
+		}
+	}
+}
