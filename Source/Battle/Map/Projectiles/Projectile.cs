c7576f8e497decb434f4a5d3e7af4f0a8ba447c4
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-23 00:22:38 +0200

    projectilemanager handle shelling

---
+++ Source/Battle/Map/Projectiles/Projectile.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+namespace Drumstalotajs.Battle.Map.Projectiles
+{
+	public partial class Projectile : Node2D
+	{
+		
+	}
+}
---
+++ Source/Battle/Map/Projectiles/ProjectileManager.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+namespace Drumstalotajs.Battle.Map.Projectiles
+{
+	public partial class ProjectileManager : Node2D
+	{
+		
+	}
+}
