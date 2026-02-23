c4ff7ce79a72182c8512d31ff52938a275510ce7
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-23 22:00:01 +0200

    Init values

--- Source/Battle/Map/Projectiles/ProjectileProperties.cs
+++ Source/Battle/Map/Projectiles/ProjectileProperties.cs
@@ -7,12 +7,27 @@ namespace Drumstalotajs.Battle.Map.Projectiles
 	{
 		public class ProjectileProperties
 		{
-			public double Altitude { get; private set; }
-			public double HVelocity { get; private set; }
+			public class AltitudeProperties
+			{
+				public double Start { get; private set; }
+				public double End { get; private set; }
+				public double Value { get; set; }
+				
+				public AltitudeProperties (double start, double end)
+				{
+					Start = start;
+					Value = start;
+					End = end;
+				}
+			}
+			
+			public AltitudeProperties Altitude { get; private set; }
+			public Vector2 Position { get; private set; }
+			public Vector2 HVelocity { get; private set; }
 			public double VVelocity { get; private set; }
-			public double Position { get; private set; }
 			
 			private Entities.Device _device;
+			private Vector2 _targetPos;
 			
 			public void NextStep()
 			{
@@ -21,11 +36,17 @@ namespace Drumstalotajs.Battle.Map.Projectiles
 			
 			public void Reset()
 			{
-				//Altitu
+				Vector2 direction = (_targetPos - _device.Position).Normalized();
+				double elevation = Battle.Physics.ToRadians(_device.Properties.Angle.Value);
+				Altitude = new AltitudeProperties(_device.Properties.Altitude, _device.Properties.Altitude);
+				Position = _device.Position;
+				HVelocity = direction * (float)(_device.Properties.MuzzleVelocity * Math.Cos(elevation));
+				VVelocity = _device.Properties.MuzzleVelocity * Math.Sin(elevation);
 			}
 			
 			public ProjectileProperties(Entities.Device device)
 			{
+				_targetPos = new Vector2(100, 100);
 				_device = device;
 				Reset();
 			}
