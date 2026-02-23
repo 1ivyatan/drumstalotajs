0df67e8fc056d66927a0b02116672ae2be6245ff
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-23 23:21:01 +0200

    calc total weight

--- Source/Battle/Entities/Device/DeviceProjectile.cs
+++ Source/Battle/Entities/Device/DeviceProjectile.cs
@@ -5,16 +5,19 @@ namespace Drumstalotajs.Battle.Entities
 {
 	public partial class Device : Entity
 	{
-		public class DeviceProjectile
+		public struct DeviceProjectile
 		{
+			public double TotalWeight { get; }
+			public double CasingWeight { get; }
+			public double ExplosiveWeight { get; }
+			public double TntFactor { get; }
+			
 			public DeviceProjectile(DeviceProperties deviceProperties, Resources.Entities.Projectile projectileData)
 			{
-				
-			/*
-		[Export] public double CasingWeight { get; set; }
-		[Export] public double ExplosiveWeight { get; set; }
-		[Export] public double TntFactor { get; set; }*/
-		
+				CasingWeight = projectileData.CasingWeight;
+				ExplosiveWeight = projectileData.ExplosiveWeight;
+				TotalWeight = CasingWeight + ExplosiveWeight;
+				TntFactor = projectileData.TntFactor;
 			}
 		}
 	}
