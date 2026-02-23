c1e6e1d31d308f6550a2d53444ff0ceec222e41f
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-23 20:49:31 +0200

    remove old comment

--- Source/Battle/Map/Projectiles/ProjectileManager.cs
+++ Source/Battle/Map/Projectiles/ProjectileManager.cs
@@ -10,19 +10,6 @@ namespace Drumstalotajs.Battle.Map.Projectiles
 		private Resources.Levels.Level _levelData;
 		private const int _maxShells = 50;
 		
-		/*
-		
-		public Projectile Fire()
-		{
-			Entities.Projectile projectile = ResourceLoader.Load<PackedScene>("res://Scenes/Battle/Entities/Devices/Projectile.tscn").Instantiate() as Projectile;
-			Resources.Levels.Level levelData = (GetNode("../../../..") as Battle.Scene).Level;
-			Vector2I gridPosition = _groundLayer.LocalToMap(Position);
-			TileData tileData = _groundLayer.GetCellTileData(gridPosition);
-			projectile.SetProjectile(Properties, Projectile, levelData, tileData);
-			_parent.AddChild(projectile);
-			return projectile;
-		}*/
-		
 		public Projectile SpawnShell(Entities.Device device)
 		{
 			if (GetChildCount() >= 50)
