13c4702d7c72dae1720ee36feb21425cd4ef7c15
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-23 23:35:57 +0200

    add more projectile properties

--- Source/Battle/Entities/Device/DeviceProjectile.cs
+++ Source/Battle/Entities/Device/DeviceProjectile.cs
@@ -7,17 +7,27 @@ namespace Drumstalotajs.Battle.Entities
 	{
 		public struct DeviceProjectile
 		{
+			public double Caliber { get; }
+			public double DragCoefficient { get; }
+			public double BallisticCoefficient { get;}
 			public double TotalWeight { get; }
 			public double CasingWeight { get; }
 			public double ExplosiveWeight { get; }
 			public double TntFactor { get; }
+			public double Area { get; }
+			public double Radius { get; }
 			
 			public DeviceProjectile(DeviceProperties deviceProperties, Resources.Entities.Projectile projectileData)
 			{
+				Caliber = projectileData.Caliber;
+				DragCoefficient = projectileData.DragCoefficient;
+				BallisticCoefficient = projectileData.BallisticCoefficient;
 				CasingWeight = projectileData.CasingWeight;
 				ExplosiveWeight = projectileData.ExplosiveWeight;
 				TotalWeight = CasingWeight + ExplosiveWeight;
 				TntFactor = projectileData.TntFactor;
+				Radius = (Caliber / 1000.0) / 2.0;
+				Area = Math.PI * Math.Pow(Radius, 2);
 			}
 		}
 	}
