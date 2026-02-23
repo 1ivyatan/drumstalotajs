fcb1efc62564c5c6ae89c0f21d47d3c940beb65d
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-23 22:44:14 +0200

    group up velocities

--- Source/Battle/Map/Projectiles/ProjectileProperties.cs
+++ Source/Battle/Map/Projectiles/ProjectileProperties.cs
@@ -21,31 +21,47 @@ namespace Drumstalotajs.Battle.Map.Projectiles
 				}
 			}
 			
+			public class VelocityProperties
+			{
+				public Vector2 Horizontal { get; private set; }
+				public double Vertical { get; private set; }
+				
+				private void ApplyHorizontalDrag(double airDensity, double delta)
+				{
+					
+				}
+				
+				private void ApplyVerticalDrag(double airDensity, double delta)
+				{
+					
+				}
+				
+				public void ApplyDrag(double airDensity, double delta)
+				{
+					ApplyHorizontalDrag(airDensity, delta);
+					ApplyVerticalDrag(airDensity, delta);
+				}
+				
+				public VelocityProperties(Vector2 direction, double muzzleVelocity, double elevationAngle)
+				{
+					Horizontal = direction * (float)(muzzleVelocity * Math.Cos(elevationAngle));
+					Vertical = muzzleVelocity * Math.Sin(elevationAngle);
+				}
+			}
+			
 			public AltitudeProperties Altitude { get; private set; }
+			public VelocityProperties Velocity { get; private set; }
 			public Vector2 Position { get; private set; }
-			public Vector2 HorizontalVelocity { get; private set; }
-			public double VerticalVelocity { get; private set; }
 			
 			private Entities.Device _device;
 			private Vector2 _targetPos;
 			
-			private void ApplyHorizontalDrag(double airDensity, double delta)
-			{
-				
-			}
-			
-			private void ApplyVerticalDrag(double airDensity, double delta)
-			{
-				
-			}
-			
 			public void NextStep(double delta)
 			{
 				double airDensity = Battle.Physics.CalculateAirDensity(Altitude.Value);
-				ApplyHorizontalDrag(airDensity, delta);
-				ApplyVerticalDrag(airDensity, delta);
-				Altitude.Value += VerticalVelocity * (float)delta;
-				Position += HorizontalVelocity * (float)delta;
+				Velocity.ApplyDrag(airDensity, delta);
+				Altitude.Value += Velocity.Vertical * (float)delta;
+				Position += Velocity.Horizontal * (float)delta;
 			}
 			
 			public void Reset()
@@ -54,8 +70,7 @@ namespace Drumstalotajs.Battle.Map.Projectiles
 				double elevation = Battle.Physics.ToRadians(_device.Properties.Angle.Value);
 				Altitude = new AltitudeProperties(_device.Properties.Altitude, _device.Properties.Altitude);
 				Position = _device.Position;
-				HorizontalVelocity = direction * (float)(_device.Properties.MuzzleVelocity * Math.Cos(elevation));
-				VerticalVelocity = _device.Properties.MuzzleVelocity * Math.Sin(elevation);
+				Velocity = new VelocityProperties(direction, _device.Properties.MuzzleVelocity, elevation);
 			}
 			
 			public ProjectileProperties(Entities.Device device)
