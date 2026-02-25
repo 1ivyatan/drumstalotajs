e55d5262e60c246d48b26893f320fe5a4e47f9ec
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-02-25 23:47:00 +0200

    add overloads

--- Source/Battle/Map/Layers/GroundLayer.cs
+++ Source/Battle/Map/Layers/GroundLayer.cs
@@ -7,9 +7,20 @@ namespace Drumstalotajs.Battle.Map.Layers
 	{
 		public double Height { get; private set; }
 		
+		public Vector2I GetCellPos(Vector2 position)
+		{
+			return LocalToMap(ToLocal(position));
+		}
+		
 		public double GetHeight(Vector2I position)
 		{
+			if (GetCellAtlasCoords(position).Equals(new Vector2I(-1, -1))) return -1.0;
 			return (double)GetCellTileData(position).GetCustomData("RelativeHeight") + Height;
+		}
+		
+		public double GetHeight(Vector2 position)
+		{
+			return GetHeight(GetCellPos(position));
 		}
 		
 		public override void _Ready()
