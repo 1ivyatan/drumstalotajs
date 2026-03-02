3ab34b28993d59a27e56f95730929609769d5936
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-02 10:53:24 +0200

    add level list function to start things out

--- Source/Levels/Scene.cs
+++ Source/Levels/Scene.cs
@@ -8,10 +8,17 @@ namespace Drumstalotajs.Levels
 		private Button _toLevelButton { get; set; }
 		private Button _toStartButton { get; set; }
 		
+		private void LoadList()
+		{
+			
+		}
+		
 		public override void _Ready()
 		{
 			_toLevelButton = GetNode<Button>("ToBattleButton");
 			_toStartButton = GetNode<Button>("ToStartButton");
+			
+			LoadList();
 			
 			_toLevelButton.Connect("pressed", Callable.From(() => {
 				(GetNode("..") as Managers.SceneManager).Battle("1");
