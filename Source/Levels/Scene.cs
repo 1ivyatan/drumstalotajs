a484c4c11ef97c08d46d99909f8c7ccbc6ae19b8
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-02 10:10:22 +0200

    add exit to start button

--- Source/Levels/Scene.cs
+++ Source/Levels/Scene.cs
@@ -6,12 +6,19 @@ namespace Drumstalotajs.Levels
 	public partial class Scene : Node
 	{
 		private Button _toLevelButton { get; set; }
+		private Button _toStartButton { get; set; }
 		
 		public override void _Ready()
 		{
 			_toLevelButton = GetNode<Button>("ToBattleButton");
+			_toStartButton = GetNode<Button>("ToStartButton");
+			
 			_toLevelButton.Connect("pressed", Callable.From(() => {
 				(GetNode("..") as Managers.SceneManager).Battle("1");
+			}));
+			
+			_toStartButton.Connect("pressed", Callable.From(() => {
+				(GetNode("..") as Managers.SceneManager).Start();
 			}));
 		}
 	}
