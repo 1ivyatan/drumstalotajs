f60d73b08db25ed507a9706888a4cca7f12a7659
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-05 16:12:25 +0200

    save before going home

--- Source/Editor/TileEditor.cs
+++ Source/Editor/TileEditor.cs
@@ -5,9 +5,24 @@ namespace Drumstalotajs.Editor
 {
 	public partial class TileEditor : Node2D
 	{
+		private TileMapLayer _groundLayer;
+		private TileMapLayer _decorationLayer;
+		
 		public override void _Ready()
 		{
+			_groundLayer = GetNode<TileMapLayer>("GroundLayer");
+			_decorationLayer = GetNode<TileMapLayer>("DecorationLayer");
 		}
 		
+		public override void _Input(InputEvent @event)
+		{
+			if (@event is InputEventKey eventKey)
+			{
+				if (eventKey.Pressed && eventKey.Keycode == Key.Escape)
+				{
+					
+				}
+			}
+		}
 	}
 }
