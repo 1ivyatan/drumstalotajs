03884adb4e2c84466a152d4fef6eb6652ce8c11b
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-05 23:15:30 +0200

    set up save

--- Source/Editor/TileEditor.cs
+++ Source/Editor/TileEditor.cs
@@ -7,19 +7,31 @@ namespace Drumstalotajs.Editor
 	{
 		private TileMapLayer _groundLayer;
 		private TileMapLayer _decorationLayer;
+		private readonly Shortcut _saveShortcut = new Shortcut();
 		
 		public override void _Ready()
 		{
 			_groundLayer = GetNode<TileMapLayer>("GroundLayer");
 			_decorationLayer = GetNode<TileMapLayer>("DecorationLayer");
+			
+			InputEventKey keyEvent = new InputEventKey
+			{
+				Keycode = Key.S,
+				CtrlPressed = true,
+				CommandOrControlAutoremap = true
+			};
+			_saveShortcut.Events = [keyEvent];
 		}
 		
 		public override void _Input(InputEvent @event)
 		{
 			if (@event is InputEventKey eventKey)
 			{
-				if (eventKey.Pressed && eventKey.Keycode == Key.Escape)
-				{
+				if (_saveShortcut.MatchesEvent(@event) &&
+					eventKey.Pressed && !eventKey.Echo
+				) {
+					GD.Print("save");
+					
 					
 				}
 			}
