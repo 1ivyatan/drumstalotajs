b9c9c9737edb7e97d9ca6c64ec7e29c174ef2397
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-08 00:57:20 +0200

    add zooming

--- Source/Scenes/BattleScene/Map/Map.cs
+++ Source/Scenes/BattleScene/Map/Map.cs
@@ -5,7 +5,18 @@ namespace Drumstalotajs.Scenes.BattleScene
 {
 	public partial class Map : Node2D
 	{
+		private Camera2D camera;
 		private bool dragging = false;
+		
+		private void Zoom(Vector2 amount)
+		{
+			camera.Zoom += amount;
+		}
+		
+		public override void _Ready()
+		{
+			camera = GetNode<Camera2D>("Camera");
+		}
 		
 		public override void _Input(InputEvent @event)
 		{
@@ -14,6 +25,32 @@ namespace Drumstalotajs.Scenes.BattleScene
 				if (mouseEvent is InputEventMouseButton mouseClick)
 				{
 					dragging = mouseClick.Pressed && mouseClick.ButtonIndex == (MouseButton)1;
+					
+					if (mouseClick.Pressed)
+					{
+						switch (mouseClick.ButtonIndex)
+						{
+							case (MouseButton)4: Zoom(new Vector2(0.25f, 0.25f)); break;
+							case (MouseButton)5: Zoom(new Vector2(-0.25f, -0.25f)); break;
+						}
+						GD.Print(mouseClick.ButtonIndex);
+					}
+					
+					/*
+					dragging = mouseClick.Pressed && buttonIndex == (MouseButton)1;
+						GD.Print(buttonIndex);
+						switch (buttonIndex)
+						{
+							case (MouseButton)4:
+								camera.Zoom += new Vector2(0.25f, 0.25f);
+								break;
+							
+							case (MouseButton)5:
+								camera.Zoom -= new Vector2(0.25f, 0.25f);
+								break;
+						}*/
+					
+					
 				}
 				
 				if (mouseEvent is InputEventMouseMotion mouseMotion)
