6391508f59d488ab38a54d2b006171fa7f47b30b
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-08 21:04:14 +0200

    fix moving

--- Source/Scenes/BattleScene/Map/MapWidget.cs
+++ Source/Scenes/BattleScene/Map/MapWidget.cs
@@ -20,22 +20,33 @@ namespace Drumstalotajs.Scenes.BattleScene.Map
 		
 		private void Move(Vector2 amount)
 		{
-			Rect2 usedRect = groundLayer.GetUsedRect();
-			Vector2 newPosition = (camera.Position - (amount / camera.Zoom));
-			Vector2 clampedPosition = newPosition.Clamp(
-				usedRect.Position * groundLayer.TileSize,
-				(usedRect.Position + usedRect.Size) * groundLayer.TileSize
-			);
-			camera.Position = clampedPosition;
-			GD.Print(clampedPosition);
-			GD.Print(usedRect.Position * groundLayer.TileSize);
-			GD.Print((usedRect.Position + usedRect.Size) * groundLayer.TileSize);
+			if (!Locked)
+			{
+				Rect2 usedRect = groundLayer.GetUsedRect();
+				Vector2 viewportSize = GetViewportRect().Size / camera.Zoom;
+				Vector2 newPosition = (camera.GlobalPosition - (amount / camera.Zoom));
+				
+				float limitLeft = camera.LimitLeft + viewportSize.X / 2;
+				float limitRight = camera.LimitRight - viewportSize.X / 2;
+				float limitTop = camera.LimitTop + viewportSize.Y / 2;
+				float limitBottom = camera.LimitBottom - viewportSize.Y / 2;
+				
+				if (limitLeft < limitRight || limitTop < limitBottom)
+				{
+					if (limitLeft < limitRight) newPosition.X = Mathf.Clamp(newPosition.X, limitLeft, limitRight);
+					if (limitTop < limitBottom) newPosition.Y = Mathf.Clamp(newPosition.Y, limitTop, limitBottom);
+					camera.GlobalPosition = newPosition;
+				}
+			}
 		}
 		
 		private void Zoom(Vector2 amount)
 		{
-			Vector2 zoom = camera.Zoom + amount;
-			camera.Zoom = zoom.Clamp(new Vector2(.25f, .25f), new Vector2(2f, 2f));
+			if (!Locked)
+			{
+				Vector2 zoom = camera.Zoom + amount;
+				camera.Zoom = zoom.Clamp(new Vector2(.25f, .25f), new Vector2(2f, 2f));
+			}
 		}
 		
 		public override void _Ready()
