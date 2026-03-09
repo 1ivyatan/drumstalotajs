1086e4aaf086b55e0c7400c14f57104ab59f2840
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-10 01:08:30 +0200

    going to bed

--- Source/Main.cs
+++ Source/Main.cs
@@ -5,5 +5,20 @@ namespace drumstalotajs;
 
 public partial class Main : Node
 {
+	private Node CurrentScene;
 	
+	public override void _Ready()
+	{
+		StartScene();
+	}
+	
+	public void StartScene()
+	{
+		LoadScene("Start");
+	}
+	
+	private void LoadScene(string name)
+	{
+		String path = $"res://Scenes/{name}/{name}.tscn";
+	}
 }
