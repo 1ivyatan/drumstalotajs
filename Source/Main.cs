e2cbdfcc1ff253960e3b8a587f459dc8fe03757f
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-10 10:03:53 +0200

    set project settings

--- Source/Main.cs
+++ Source/Main.cs
@@ -5,20 +5,11 @@ namespace drumstalotajs;
 
 public partial class Main : Node
 {
-	private Node CurrentScene;
+	private Managers.SceneManager sceneManager;
 	
 	public override void _Ready()
 	{
-		StartScene();
-	}
-	
-	public void StartScene()
-	{
-		LoadScene("Start");
-	}
-	
-	private void LoadScene(string name)
-	{
-		String path = $"res://Scenes/{name}/{name}.tscn";
+		sceneManager = GetNode<Node>("SceneManager") as Managers.SceneManager;
+		sceneManager.StartScene();
 	}
 }
--- Source/Managers/SceneManager.cs
+++ Source/Managers/SceneManager.cs
@@ -1,6 +1,19 @@
 using Godot;
 using System;
 
+namespace drumstalotajs.Managers;
+
 public partial class SceneManager : Node
 {
+	private Node CurrentScene;
+	
+	public void StartScene()
+	{
+		LoadScene("Start");
+	}
+	
+	private void LoadScene(string name)
+	{
+		String path = $"res://Scenes/{name}/{name}.tscn";
+	}
 }
