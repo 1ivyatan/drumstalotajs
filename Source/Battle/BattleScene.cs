84333100d15c1057d31f360756d64b45178c2153
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-23 22:21:06 +0200

    add states

--- Source/Battle/BattleScene.cs
+++ Source/Battle/BattleScene.cs
@@ -8,15 +8,32 @@ public partial class BattleScene : Node2D
 	private Resources.Maps.Meta MetaData { get; set; }
 	
 	private Mapping.Map map;
+	private Managers.CursorManager cursorManager;
 
 	public override void _Ready()
 	{
 		map = GetNode<Node2D>("Map") as Mapping.Map;
+		cursorManager = GetNode<Node>("../../CursorManager") as Managers.CursorManager;
 		map.LoadMap(MetaData);
 		map.Mode = Mapping.Map.MapMode.VIEW;
 		
-		/* cursor */
 		map.Camera.StateChange += (Mapping.Camera.MapCamera.MapCameraState state) => {
+			switch (state)
+			{
+				case Mapping.Camera.MapCamera.MapCameraState.IDLE:
+					cursorManager.ArrowCursor();
+					break;
+				case Mapping.Camera.MapCamera.MapCameraState.ZOOM:
+				case Mapping.Camera.MapCamera.MapCameraState.ZOOMDRAG:
+					cursorManager.ZoomCursor();
+					break;
+				case Mapping.Camera.MapCamera.MapCameraState.DRAG:
+					cursorManager.DragCursor();
+					break;
+				default:
+					cursorManager.ResetCursor();
+					break;
+			}
 			GD.Print(state);
 		};
 	}
--- Source/Managers/CursorManager.cs
+++ Source/Managers/CursorManager.cs
@@ -6,6 +6,25 @@ namespace drumstalotajs.Managers;
 
 public partial class CursorManager : Node
 {
+	[Export] private Texture2D zoomCursorTexture;
+	
+	public void DragCursor()
+	{
+		ResetCursor();
+		Input.SetDefaultCursorShape(Input.CursorShape.Move);
+	}
+	
+	public void ArrowCursor()
+	{
+		ResetCursor();
+		Input.SetDefaultCursorShape(Input.CursorShape.Arrow);
+	}
+	
+	public void ZoomCursor()
+	{
+		Input.SetCustomMouseCursor(zoomCursorTexture);
+	}
+
 	public void ResetCursor()
 	{
 		Input.SetCustomMouseCursor(null);
