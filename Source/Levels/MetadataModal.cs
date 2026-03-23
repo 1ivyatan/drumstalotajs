2a68fa4eb799ecf0c9309ba60eb01762e8baae55
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-03-23 14:43:20 +0200

    add the modal

--- Source/Levels/MetadataModal.cs
+++ Source/Levels/MetadataModal.cs
@@ -5,8 +5,25 @@ namespace drumstalotajs.Levels;
 
 public partial class MetadataModal : Control
 {
+	private Resources.Sets.Levels.LevelProperties LevelProperties { get; set; }
+
+	private Label title;
+	private RichTextLabel desc;
+	private Button toBattleButton;
+	
+	public override void _Ready()
+	{
+		Container container = GetNode<Container>("VBoxContainer");
+		title = container.GetNode<Label>("Title");
+		desc = container.GetNode<RichTextLabel>("Desc");
+		toBattleButton = container.GetNode<Button>("ToBattleButton");
+	}
+	
 	public void LoadModal(Resources.Sets.Levels.LevelProperties levelProps)
 	{
+		LevelProperties = levelProps;
+		title.Text = levelProps.Meta.Title;
+		desc.Text = levelProps.Meta.Desc;
 		Visible = true;
 	}
 	
