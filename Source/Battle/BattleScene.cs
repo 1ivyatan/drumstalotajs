c05bc950d0cf7c88c55eb8ca0547f544df56a819
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-03 19:25:12 +0300

    unused removal

--- Source/Battle/BattleScene.cs
+++ Source/Battle/BattleScene.cs
@@ -56,39 +56,3 @@ public partial class BattleScene : Node2D
 		
 	
 }
-/*
-
-
-	public void PauseScene() { if (State != SceneState.LOADING) State = SceneState.RUNNING; }
-	public void ResumeScene() { if (State != SceneState.LOADING) State = SceneState.RUNNING; }
-using Godot;
-using System;
-using Drumstalotajs;
-using Drumstalotajs.Utils;
-using Drumstalotajs.Components.Modals;
-
-namespace Drumstalotajs.Start;
-
-public partial class StartScene : Control
-{
-	private Modal _modal;
-	private Button _startButton;
-	private Button _annotationButton;
-	private Button _quitButton;
-
-	public override void _Ready()
-	{
-		Node buttons = GetNode("Grid/MenuColumn/Buttons");
-		_modal = GetNode("AnnotationModal") as Modal;
-		_startButton = buttons.GetNode<Button>("Start");
-		_annotationButton = buttons.GetNode<Button>("Annotation");
-		_quitButton = buttons.GetNode<Button>("Quit");
-		_startButton.Pressed += () => {
-			Nodes.GetRoot().SceneManager.LevelSelection();
-		};
-		_annotationButton.Pressed += () => {
-			_modal.Show();
-		};
-	}
-}
-*/
--- Source/Resources/Progress/LevelProgress.cs
+++ Source/Resources/Progress/LevelProgress.cs
@@ -23,28 +23,6 @@ public partial class LevelProgress : Resource
 		}
 	}
 	
-	public bool IsUnlocked(LevelSetProps levelProps)
-	{
-		var matches = CompletedScores.Where(s => s.LevelProps == levelProps).ToArray();
-		if (matches.Length > 0)
-		{
-			return true;
-		} else if (matches.Length == 0 && levelProps.Order == 1)
-		{
-			return true;
-		}
-		
-		var lastScore = CompletedScores.LastOrDefault();
-		
-		if (lastScore != null && (levelProps.Order - lastScore.LevelProps.Order) == 1)
-		{
-			return true;
-		} else
-		{
-			return false;
-		}
-	}
-	
 	public ProgressState GetProgressState(LevelSetProps levelProps)
 	{
 		var matches = CompletedScores.Where(s => s.LevelProps == levelProps).ToArray();
