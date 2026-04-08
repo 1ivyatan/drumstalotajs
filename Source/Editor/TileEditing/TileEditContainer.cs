cdf6e352b92daebe3e9bbdba4ab667b8dd1a2250
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-08 15:27:22 +0300

    add classes

--- Source/Editor/TileEditing/TileEditContainer.cs
+++ Source/Editor/TileEditing/TileEditContainer.cs
@@ -8,16 +8,23 @@ namespace Drumstalotajs.Editor.TileEditing;
 public partial class TileEditContainer : Control
 {
 	private Map _map;
+	private GroundEditor _groundEditor;
+	private EntityEditor _entityEditor;
 	
 	public override void _Ready()
 	{
 		_map = Nodes.GetSceneRoot().Map;
+		_groundEditor = GetNode("VBoxContainer/GroundEditor") as GroundEditor;
+		_entityEditor = GetNode("VBoxContainer/EntityEditor") as EntityEditor;
 	}
 	
 	public void Open(Vector2I position)
 	{
 		_map = Nodes.GetSceneRoot().Map;
 		if (_map.IsEmptyTile(position)) return;
+		
+		
+		
 		Visible = true;
 	}
 	
