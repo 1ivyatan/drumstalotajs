13696e7ae7b1911e7c64e7e7f0519e33120f3204
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-08 23:13:54 +0300

    set the heights

--- Source/Editor/TileEditing/GroundEditor.cs
+++ Source/Editor/TileEditing/GroundEditor.cs
@@ -10,6 +10,7 @@ public partial class GroundEditor : Control
 	private Map _map;
 	private Label _relHeight;
 	private SpinBox _heightChanger;
+	private Vector2I _selectedPosition;
 	
 	public override void _Ready()
 	{
@@ -17,12 +18,22 @@ public partial class GroundEditor : Control
 		var heightEditor = GetNode("HeightEditor");
 		_relHeight = heightEditor.GetNode<Label>("RelHeight");
 		_heightChanger = heightEditor.GetNode<SpinBox>("HeightChanger");
+		
+		_heightChanger.ValueChanged += (double value) => {
+			Save();
+		};
 	}
 	
 	public void Load(Vector2I position)
 	{
 		_map = Nodes.GetSceneRoot().Map;
+		_selectedPosition = position;
 		_relHeight.Text = $"{_map.GroundLayer.GetUnaddedHeight(position)}+";
 		_heightChanger.Value = _map.GroundLayer.GetAddedHeight(position);
+	}
+	
+	public void Save()
+	{
+		_map.GroundLayer.SetAddedHeight(_selectedPosition, _heightChanger.Value);
 	}
 }
