dfa406b1283aa307e9411c0b6d880d49805fb4f7
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-10 00:31:17 +0300

    rename again

--- Source/Start/StartScene.cs
+++ Source/Start/StartScene.cs
@@ -9,7 +9,7 @@ public partial class StartScene : Control
 {
 	private Modal _modal;
 	private Button _startButton;
-	private Button _annotationButton;
+	private Button _aboutButton;
 	private Button _quitButton;
 
 	public override void _Ready()
@@ -17,9 +17,9 @@ public partial class StartScene : Control
 		Node buttons = GetNode("Grid/MenuColumn/Buttons");
 		_modal = GetNode("AnnotationModal") as Modal;
 		_startButton = buttons.GetNode<Button>("Start");
-		_annotationButton = buttons.GetNode<Button>("Annotation");
+		_aboutButton = buttons.GetNode<Button>("About");
 		_quitButton = buttons.GetNode<Button>("Quit");
-		_annotationButton.Pressed += () => {
+		_aboutButton.Pressed += () => {
 			_modal.Show();
 		};
 	}
