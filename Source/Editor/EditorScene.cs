258754fd8071ba0a4939f63be9a5ae9c1057e89b
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-10 16:50:20 +0300

    set the switch

--- Source/Editor/EditorScene.cs
+++ Source/Editor/EditorScene.cs
@@ -12,6 +12,7 @@ namespace Drumstalotajs.Editor;
 public partial class EditorScene : Node2D
 {
 	[Export] private Topnav _topnav;
+	[Export] private TileSelectionContainer _tileSelectionContainer;
 	[Export] public Map Map;
 	public EditorMode Mode { get; private set; } = EditorMode.View;
 	
@@ -21,10 +22,34 @@ public partial class EditorScene : Node2D
 		_topnav.SelectedExit += () => { Nodes.GetRoot().SceneManager.Start(); };
 		_topnav.SelectedMode += (EditorMode mode) => { 
 			Mode = mode;
+			switch (mode)
+			{
+				case EditorMode.View:
+					_tileSelectionContainer.Visible = false;
+					break;
+				case EditorMode.Edit:
+					_tileSelectionContainer.Visible = false;
+					break;
+				case EditorMode.Insert:
+					_tileSelectionContainer.Visible = true;
+					break;
+				default: break;
+			}
 		};
 		/*cccccc*/
 		Map.Camera.SetCalibratingAtlasLayer(Map.GroundLayer);
 		Map.Camera.Mode = CameraMode.View;
 		Map.Camera.Calibrate();
+	}
+	
+	public async override void _UnhandledInput(InputEvent @event)
+	{
+		switch (Mode)
+		{
+			case EditorMode.Insert:
+				
+				break;
+			default: break;
+		}
 	}
 }
