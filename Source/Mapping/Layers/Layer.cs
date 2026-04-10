2fccffccde7c0c15fd7adac75fb493d167d096ee
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-10 12:38:23 +0300

    set up map component

---
+++ Source/Mapping/Layers/Layer.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+namespace Drumstalotajs.Mapping.Layers;
+
+public abstract partial class Layer<TAtlasType> : TileMapLayer
+{
+	
+}
---
+++ Source/Mapping/Map.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+using Drumstalotajs;
+using Drumstalotajs.Utils;
+
+namespace Drumstalotajs.Mapping;
+
+public partial class Map : Node2D
+{
+	
+}
