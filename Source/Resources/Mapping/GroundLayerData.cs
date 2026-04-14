c1993ae33c0e90f78b4c57984db092cad0da5016
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-14 16:22:13 +0300

    add height

--- Source/Resources/Mapping/GroundLayerData.cs
+++ Source/Resources/Mapping/GroundLayerData.cs
@@ -7,9 +7,11 @@ namespace Drumstalotajs.Resources.Mapping;
 [GlobalClass]
 public partial class GroundLayerData : AtlasLayerData
 {
+	[Export] public double BaseHeight { get; private set; }
 	[Export] public AddedGroundHeightAtlas AddedHeights { get; private set; }
 	public GroundLayerData(GroundLayer layer) : base(layer)
 	{
+		BaseHeight = layer.BaseHeight;
 		AddedHeights = layer.AddedHeights;
 	}
 }
