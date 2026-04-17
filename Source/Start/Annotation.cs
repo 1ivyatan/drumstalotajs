4dd1162f80096cf79aafe14ed39df0b4608cbef2
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-17 15:50:56 +0300

    set up annotation

--- Source/Start/Annotation.cs
+++ Source/Start/Annotation.cs
@@ -7,21 +7,24 @@ namespace Drumstalotajs.Start;
 
 public partial class Annotation : Components.Modals.Window
 {
-	[Export] private string _annotationFilePath;
-	
-	private Button _close;
-	private RichTextLabel _text;
+	[Export(PropertyHint.File, "*.txt")] private string _annotationFilePath;
+	[Export] private Button _close;
+	[Export] private RichTextLabel _text;
 	
 	public override void _Ready()
 	{
-		_close = GetNode<Button>("Close");
-		_text = GetNode<RichTextLabel>("Text");
-		
 		var annFile = Files.SafeLoadFile(_annotationFilePath,  FileAccess.ModeFlags.Read);
 		if (annFile != null)
 		{
 			_text.Text = annFile.GetAsText();
 		}
+		
+		_text.MetaClicked += (Variant meta) => {
+			if (meta.VariantType == Variant.Type.String)
+			{
+				OS.ShellOpen(meta.ToString());
+			}
+		};
 		
 		_close.Pressed += () => {
 			GetModal().HideModal();
