1509f1701da1033ae8553c453029405791a6a1c2
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-19 23:12:15 +0300

    add in rotation

--- Source/Mapping/Layers/AtlasLayer.cs
+++ Source/Mapping/Layers/AtlasLayer.cs
@@ -42,6 +42,28 @@ public partial class AtlasLayer : Layer<Vector2I, AtlasTile, AtlasLayerData>
 		EmitSignal(SignalName.ChangedLayer);
 	}
 	
+	public void RotateTile(Vector2I position, double degrees)
+	{
+		Vector2I atlasCoords = GetCellAtlasCoords(position);
+		if (atlasCoords == Types.Vector2I.Negative) return;
+		int bitwise = 0;
+		int quadrant = Calculations.GetQuadrant(degrees);
+		
+		switch (quadrant)
+		{
+			case 1: bitwise = 0; break;
+			case 2: bitwise = (int)(TileSetAtlasSource.TransformTranspose | 
+			TileSetAtlasSource.TransformFlipH); break;
+			case 3: bitwise = (int)(TileSetAtlasSource.TransformFlipH | 
+			TileSetAtlasSource.TransformFlipV); break;
+			case 4: bitwise = (int)(TileSetAtlasSource.TransformTranspose | 
+			TileSetAtlasSource.TransformFlipV); break;
+			default: break;
+		}
+		
+		SetCell(position, 0, atlasCoords, bitwise);
+	}
+	
 	public override Godot.Collections.Array<AtlasTile> Flash(Vector2I position)
 	{
 		if (GetCellAtlasCoords(position) == Types.Vector2I.Negative) return [];
---
+++ Source/Utils/Calculations.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Drumstalotajs.Utils;
+
+public static class Calculations
+{
+	/*
+		Thanks for this:
+		https://stackoverflow.com/questions/13975745/the-fastest-way-to-get-current-quadrant-of-an-angle
+	*/
+	
+	public static int GetQuadrant(double degrees)
+	{
+		degrees %= 360.0;
+		if (degrees < 0) degrees += 360.0;
+		return ((int)degrees/90) % 4 + 1;
+	}
+}
