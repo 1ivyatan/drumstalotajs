699b4dfe1151f8ff728a6588daf88594d2a30dce
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-19 19:50:08 +0300

    set up templating

---
+++ Source/Templates/Node/Base.cs
@@ -0,0 +1,16 @@
+using _BINDINGS_NAMESPACE_;
+using System;
+using Drumstalotajs;
+
+namespace Drumstalotajs;
+
+public partial class _CLASS_ : _BASE_
+{
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
