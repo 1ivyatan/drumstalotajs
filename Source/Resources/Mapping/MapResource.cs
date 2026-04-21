364d595438e26a8abab30c3c14c25592456a94ba
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-21 12:55:17 +0300

    add in more layers to export

--- Source/Resources/Mapping/MapResource.cs
+++ Source/Resources/Mapping/MapResource.cs
@@ -12,10 +12,14 @@ public partial class MapResource : Resource
 {
 	[ExportGroup("Layers")]
 	[Export] GroundLayerData GroundLayer { get; set; } = null;
-	
+	[Export] AtlasLayerData DecorationLayer { get; set; } = null;
+	[Export] EntityLayerData EntityLayer { get; set; } = null;
+	[Export] OverlayLayerData OverlayLayer { get; set; } = null;
+
 	public MapResource () {}
 	public MapResource (Map map)
 	{
-		
+		GroundLayer = map.GroundLayer.Export();
+		DecorationLayer = map.DecorationLayer.Export();
 	}
 }
