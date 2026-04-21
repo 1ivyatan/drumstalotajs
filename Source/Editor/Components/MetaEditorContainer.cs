8df996f18660f31d93045a918926064a50c19c0d
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-21 18:20:38 +0300

    line edit

--- Source/Editor/Components/MetaEditorContainer.cs
+++ Source/Editor/Components/MetaEditorContainer.cs
@@ -9,4 +9,13 @@ namespace Drumstalotajs.Editor.Components;
 
 public partial class MetaEditorContainer : ModalWindow
 {
+	[Export] private LineEdit _title;
+	public string Title { get; private set; }
+	
+	public override void _Ready()
+	{
+		_title.TextChanged += (string value) => {
+			Title = value;
+		}; 
+	}
 }
