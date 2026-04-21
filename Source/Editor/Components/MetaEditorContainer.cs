c66de2a7b39c8553dc85ddea005cd92128fefd35
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-21 17:53:24 +0300

    touches

--- Source/Editor/Components/MetaEditorContainer.cs
+++ Source/Editor/Components/MetaEditorContainer.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
-/* using Drumstalotajs;
+using Drumstalotajs;
 using Drumstalotajs.Editor;
 using Drumstalotajs.Components;
 
@@ -8,26 +8,4 @@ namespace Drumstalotajs.Editor.Components;
 
 public partial class MetaEditorContainer : Control
 {
-	[Signal] public delegate void ExitingEventHandler();
-	[Export] public MetaEditor MetaEditor { get; set; }
-	
-	public override void _Ready()
-	{
-		MetaEditor.CloseRequested += () => {
-			EmitSignal(SignalName.Exiting);
-		};
-	}
-	
-	public void Open()
-	{
-		Visible = true;
-		MetaEditor.OpenCentered();
-	}
-	
-	public void Close()
-	{
-		Visible = false;
-		MetaEditor.Hide();
-	}
 }
-*/
