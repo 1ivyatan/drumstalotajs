6407e83b29bb1579aa21c5c4d08174676dd5ffc4
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-04-29 00:17:04 +0300

    add meta container

--- Source/LevelSelection/Components/LevelMetaContainer.cs
+++ Source/LevelSelection/Components/LevelMetaContainer.cs
@@ -7,17 +7,20 @@ namespace Drumstalotajs.LevelSelection.Components;
 
 public partial class LevelMetaContainer : Control
 {
-	[Export] private Button _loadCustomLevel;
+	[Export] private Button _loadCustomMap;
+	[Export] private FileDialog _customMapDialog;
 	
 	public override void _Ready()
 	{
-		Utilities.Editor.EditorControl(_loadCustomLevel);
-		
-		_loadCustomLevel.Pressed += () => {
-			if (Utilities.Editor.IsEditor())
-			{
-				
-			}
-		};
+		Utilities.Editor.EditorControl(_loadCustomMap);
+		if (Utilities.Editor.IsEditor())
+		{
+			_customMapDialog.FileSelected += (string path) => {
+				GD.Print(path);
+			};
+			_loadCustomMap.Pressed += () => {
+				_customMapDialog.PopupCentered();
+			};
+		}
 	}
 }
