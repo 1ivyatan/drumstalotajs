7e93623f456132df227d68ed1c497ee8c3c09de4
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-05-01 17:12:39 +0300

    add in fade effect

--- Source/Managers/Scenes/FadeCurtainContainer.cs
+++ Source/Managers/Scenes/FadeCurtainContainer.cs
@@ -7,16 +7,24 @@ namespace Drumstalotajs.Managers.Scenes;
 
 public partial class FadeCurtainContainer : CanvasLayer
 {
+	[Export] private double Time { get; set; }
+	[Export] private Color FadeInColor { get; set; }
+	[Export] private Color FadeOutColor { get; set; }
+	[Export] private ColorRect ColorRect { get; set; }
+	
 	public async Task FadeIn()
 	{
-		
+		Tween tween = GetTree().CreateTween();
+		Visible = true;
+		tween.TweenProperty(ColorRect, "color", FadeInColor, (float)Time);
+		await ToSignal(tween, Tween.SignalName.Finished);
 	}
 	
 	public async Task FadeOut()
 	{
-		
+		Tween tween = GetTree().CreateTween();
+		tween.TweenProperty(ColorRect, "color", FadeOutColor, (float)Time);
+		tween.TweenCallback(Callable.From(() => { Visible = false; }));
+		await ToSignal(tween, Tween.SignalName.Finished);
 	}
-	
-			/*
-	await ToSignal(tween, Tween.SignalName.Finished);*/
 }
