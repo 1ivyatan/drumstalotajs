342830916c49a5b9ada331d64ee4925e40d7fc2f
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-05-02 23:59:28 +0300

    stage man and more deco tiles

---
+++ Source/Battle/Stages/StageManager.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+using Drumstalotajs;
+
+namespace Drumstalotajs.Battle.Stages;
+
+public partial class StageManager : Node
+{
+	public override void _Ready()
+	{
+	}
+}
