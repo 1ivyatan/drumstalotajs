1f4526bd7fc4188d21ccbf112f59fbf46de83cab
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-05-03 13:12:06 +0300

    prepare inputting

--- Source/Battle/Stages/DevicePlacement.cs
+++ Source/Battle/Stages/DevicePlacement.cs
@@ -3,14 +3,24 @@ using System;
 using Drumstalotajs;
 using Drumstalotajs.Utilities;
 using Drumstalotajs.Battle;
+using Drumstalotajs.Mapping;
 
 namespace Drumstalotajs.Battle.Stages;
 
 public partial class DevicePlacement : Control
 {
+	private BattleScene _scene;
+	private Map _map;
+	
 	public override void _Ready()
 	{
-		var sceneRoot = Nodes.GetSceneRoot();
-		sceneRoot.BattleTopnav.Title = "Device placement";
+		_scene = Nodes.GetSceneRoot() as BattleScene;
+		_map = _scene.Map;
+		_scene.BattleTopnav.Title = "Device placement";
+	}
+	
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		GD.Print(3333);
 	}
 }
