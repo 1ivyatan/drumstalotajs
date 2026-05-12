b7eafaabe63904bdc619fb74e5fda9ac9afaa412
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-05-13 00:00:15 +0300

    animate overlays

--- Source/Mapping/Overlays/DeviceMarker.cs
+++ Source/Mapping/Overlays/DeviceMarker.cs
@@ -17,7 +17,35 @@ public partial class DeviceMarker : OverlayTile
 //			field = value;
 //		}
 //	} = new();
-
+/*
+	[Export] private float _speed = 1.0f;
+	private float _t = 0f;
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		_t += (float)delta * _speed;
+		float weight = Mathf.PingPong(_t, 1f);
+		Modulate = _c1.Lerp(_c2, weight);
+	}*/
+	
+	[Export] private float _speed = 10.0f;
+	private float _t = 0f;
+	private Vector2 _ogArrowPos;
+	
+	public override void _Ready()
+	{
+		_ogArrowPos = _arrow.Offset;
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		_t += (float)delta * _speed;
+		float weight = Mathf.PingPong(_t, 10f);
+		var newPos = _ogArrowPos;
+		newPos.Y = newPos.Y - weight;
+		_arrow.Offset = newPos;
+	}
+	
 	public void SetArrowRotation(double degrees)
 	{
 		_arrow.RotationDegrees = (float)degrees;
--- Source/Mapping/Overlays/SelectorHighlight.cs
+++ Source/Mapping/Overlays/SelectorHighlight.cs
@@ -10,4 +10,16 @@ namespace Drumstalotajs.Mapping.Overlays;
 
 public partial class SelectorHighlight : OverlayTile
 {
+	[Export] private Color _c1;
+	[Export] private Color _c2;
+	
+	[Export] private float _speed = 1.0f;
+	private float _t = 0f;
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		_t += (float)delta * _speed;
+		float weight = Mathf.PingPong(_t, 1f);
+		Modulate = _c1.Lerp(_c2, weight);
+	}
 }
