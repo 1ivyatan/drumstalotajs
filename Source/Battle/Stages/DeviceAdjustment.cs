bb1898b1aad9b69c4cad6e04dcdf3df18b6ed61d
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-05-18 02:19:53 +0300

    prevent editing destroyed devices

--- Source/Battle/Stages/DeviceAdjustment.cs
+++ Source/Battle/Stages/DeviceAdjustment.cs
@@ -72,7 +72,7 @@ public partial class DeviceAdjustment : Control
 				if (tiles.Count > 0)
 				{
 					var tile = tiles[_map.EntityLayer][0];
-					if ((SceneTile)tile is Device device)
+					if ((SceneTile)tile is Device device && !device.Disabled)
 					{
 						if (!device.Player && !Utilities.Editor.IsEditor()) return;
 						_map.OverlayLayer.ClearAllHighlighters();
