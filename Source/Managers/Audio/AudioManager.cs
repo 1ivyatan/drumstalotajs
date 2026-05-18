32fb6dc2d56e64ec35f521bea6a2714d9a9823c5
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-05-19 01:14:07 +0300

    add the class

---
+++ Source/Managers/Audio/AudioManager.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Drumstalotajs.Utilities;
+
+namespace Drumstalotajs.Managers.Audio;
+
+public partial class AudioManager : Node
+{
+	public override void _Ready()
+	{
+	}
+}
