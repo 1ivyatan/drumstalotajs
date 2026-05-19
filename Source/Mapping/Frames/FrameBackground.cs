802e943f1e6780d47146926714b01881d7f2ae63
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-05-19 22:50:42 +0300

    add the black border

--- Source/Mapping/Frames/FrameBackground.cs
+++ Source/Mapping/Frames/FrameBackground.cs
@@ -5,22 +5,18 @@ using Drumstalotajs.Mapping.Layers;
 
 namespace Drumstalotajs.Mapping.Entities;
 
-public partial class FrameBackground : Sprite2D
+public partial class FrameBackground : Control
 {
 	[Export] private AtlasLayer _calibratingAtlasLayer;
+	[Export] private Vector2 Padding { get; set; }
 
 	public void Calibrate()
 	{
 		Rect2 usedRect = _calibratingAtlasLayer.GetUsedRect();
 		int tileSize = _calibratingAtlasLayer.TileSize;
-		GD.Print(usedRect);
-		/*
-		Rect2 usedRect = _calibratingAtlasLayer.GetUsedRect();
-		int tileSize = _calibratingAtlasLayer.TileSize;
-		LimitLeft = (int)(usedRect.Position.X * tileSize);
-		LimitRight = (int)((usedRect.Position.X + usedRect.Size.X) * tileSize);
-		LimitTop = (int)(usedRect.Position.Y * tileSize); //- (int)();
-		LimitBottom = (int)((usedRect.Size.Y * tileSize) + (usedRect.Position.Y * tileSize));
-		GlobalPosition = usedRect.Position + usedRect.GetCenter() * _calibratingAtlasLayer.TileSize;*/
+		var width = tileSize * usedRect.Size.X;
+		var height = tileSize * usedRect.Size.Y;
+		Size = new Vector2(width, height) + Padding;
+		Position = (usedRect.Position * tileSize) - (Padding / 2);
 	}
 }
