ad1d058359902364a6e77036e358ac7a1d1c9c3a
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-06-03 23:38:35 +0300

    grab r click

--- Source/Battle/Components/RulerOverlay.cs
+++ Source/Battle/Components/RulerOverlay.cs
@@ -9,9 +9,33 @@ public partial class RulerOverlay : Control
 	public override void _Ready()
 	{
 	}
-
+	
+	
+	
 	public override void _Input(InputEvent @event)
 	{
 		if (!Visible) return;
+		
+		if (@event is InputEventMouse mouseEvent)
+		{
+			bool moving = false;
+			
+			if (mouseEvent is InputEventMouseMotion mouseMotion)
+			{
+				moving = true;
+			}
+			
+			if (mouseEvent is InputEventMouseButton mouseButton && 
+				mouseButton.Pressed && !moving
+			)
+			{
+				if (mouseButton.ButtonIndex == MouseButton.Right)
+				{
+					GD.Print(222);
+				}
+				/*
+				mouseButton.ButtonIndex == MouseButton.Left &&*/
+			}
+		}
 	}
 }
