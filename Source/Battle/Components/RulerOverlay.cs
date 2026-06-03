d16aec065e0a0c8672442cdbe17a7080bc8dbf3b
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-06-04 00:55:32 +0300

    deselect button

--- Source/Battle/Components/RulerOverlay.cs
+++ Source/Battle/Components/RulerOverlay.cs
@@ -18,7 +18,10 @@ public partial class RulerOverlay : Control
 	
 	public override void _Ready()
 	{
-		_deselect.Pressed += () => { _adding = false; };
+		_deselect.Pressed += () => { 
+			_adding = false;
+			_deselect.Disabled = true;
+		};
 	}
 	
 	public override void _Input(InputEvent @event)
@@ -50,16 +53,19 @@ public partial class RulerOverlay : Control
 						{
 							_adding = true;
 							_pointA = pos;
+							_deselect.Disabled = false;
 							GD.Print($"1. {pos}");
 						} else
 						{
 							_pointB = pos;
 							_adding = false;
+							_deselect.Disabled = true;
 							GD.Print($"2. {pos}");
 						}
 					} else
 					{
 						_adding = false;
+						_deselect.Disabled = true;
 					}
 				}
 			}
