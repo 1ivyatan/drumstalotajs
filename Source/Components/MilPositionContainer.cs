bc7d50da74bc7d9198d0b6d8891f4c5606fe46fc
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-06-05 12:36:40 +0300

    turn it into a togglable

--- Source/Components/MilPositionContainer.cs
+++ Source/Components/MilPositionContainer.cs
@@ -8,16 +8,58 @@ using Drumstalotajs.Mapping;
 
 namespace Drumstalotajs.Components;
 
-public partial class MilPositionContainer : Control
+public partial class MilPositionContainer : BaseButton
 {
 	[Export] private Label _posText;
 	[Export] private Map _map;
 	private bool _precise = false;
+	private Vector2 _oldPos;
+	
+	public override void _Ready()
+	{
+		Toggled += (bool on) => {
+			_precise = on;
+			UpdateCoords(_oldPos);
+		};
+	}
+	
+	public void UpdateCoords(Vector2 coords)
+	{
+		if (_map != null)
+		{
+			if (_precise)
+			{
+				var pos = coords.Snapped(new Vector2(0.01f, 0.01f));
+				var str = _map.FormatMilCoords(coords);
+				_oldPos = pos;
+				_posText.Text = str;
+			} else
+			{
+				var pos = coords;
+				var str = _map.FormatMilCoords((Vector2I)pos);
+				_oldPos = pos;
+				_posText.Text = str;
+			}
+		}
+	}
 	
 	public void UpdateCoords()
 	{
-		var coords = _map.ViewportToMilCoords();
-		var str = _map.FormatMilCoords((Vector2I)coords);
-		_posText.Text = str;
+		if (_map != null)
+		{
+			if (_precise)
+			{
+				var coords = _map.ViewportToMilCoords().Snapped(new Vector2(0.01f, 0.01f));
+				var str = _map.FormatMilCoords(coords);
+				_oldPos = coords;
+				_posText.Text = str;
+			} else
+			{
+				var coords = _map.ViewportToMilCoords();
+				var str = _map.FormatMilCoords((Vector2I)coords);
+				_oldPos = coords;
+				_posText.Text = str;
+			}
+		}
 	}
 }
