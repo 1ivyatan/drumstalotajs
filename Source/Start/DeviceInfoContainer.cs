daa530fdd6bbb82307ce1b9cc3237262d68697d9
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-06-06 16:23:14 +0300

    add carousel logic

--- Source/Start/DeviceInfoContainer.cs
+++ Source/Start/DeviceInfoContainer.cs
@@ -21,13 +21,40 @@ public partial class DeviceInfoContainer : Control
 	[Export] private Button _left;
 	[Export] private Button _right;
 	
+	private int _idx = 0;
+	
 	public override void _Ready()
 	{
 		_close.Pressed += Close;
+		_left.Pressed += () => {
+			Prev();
+		};
+		_right.Pressed += () => {
+			Next();
+		};
+	}
+	
+	private void Next()
+	{
+		int newIdx = _idx + 1 >= _devices.Length ? 0 : _idx + 1;
+		LoadDeviceInfo(newIdx);
+	}
+	
+	private void Prev()
+	{
+		int newIdx = _idx - 1 < 0 ? _devices.Length - 1 : _idx - 1;
+		LoadDeviceInfo(newIdx);
+	}
+	
+	private void LoadDeviceInfo(int idx)
+	{
+		_idx = idx;
+		//GD.Print(_idx);
 	}
 	
 	public void Open()
 	{
+		LoadDeviceInfo(0);
 		Visible = true;
 	}
 	
